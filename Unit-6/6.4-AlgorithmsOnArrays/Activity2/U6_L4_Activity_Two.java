8ed08ef9d643d171c1f6726ce5b853f4cb9e939f
coderunnerone1 <admin@coderunner.me>
2023-12-15 11:43:15 -0800

    Update U6_L4_Activity_Two.java

--- Unit-6/6.4-AlgorithmsOnArrays/Activity2/U6_L4_Activity_Two.java
+++ Unit-6/6.4-AlgorithmsOnArrays/Activity2/U6_L4_Activity_Two.java
@@ -2,10 +2,8 @@ public class U6_L4_Activity_Two
 {
   public static void swap(int[] arr, int i, int j)
   {
-    // Check i & j
     if (i >= 0 && j >= 0)
     {
-      // Store into temp. array
       int[] tempNum = {arr[i], arr[j]};
       // Write to arr
       arr[i] = tempNum[1];
@@ -15,7 +13,6 @@ public class U6_L4_Activity_Two
   
   public static void allReverseSwap(int[] arr)
   {
-    // Store into temp. array
     int[] tempArr = new int[arr.length];
     for (int i = 0; i < arr.length; i++)
     {
