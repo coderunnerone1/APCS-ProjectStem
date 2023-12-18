680e8a7a24526177e98dc6f468522d6f63b40392
coderunnerone1 <admin@coderunner.me>
2023-12-18 11:36:15 -0800

    Update U6_L5_Activity_Three.java
    0_0

--- Unit-6/6.5-TheEnhancedForLoop/Activity3/U6_L5_Activity_Three.java
+++ Unit-6/6.5-TheEnhancedForLoop/Activity3/U6_L5_Activity_Three.java
@@ -2,16 +2,12 @@ public class U6_L5_Activity_Three
 {
   public static double avg(int[] arr)
   {
-    // Create Variable
     double s = 0;
-    // For Each Int in arr[]
     for (double n : arr)
     {
       s += n;
     }
-    // Divide by arr[] Length
     s /= arr.length;
-    // End
     return s;
   }
 }
