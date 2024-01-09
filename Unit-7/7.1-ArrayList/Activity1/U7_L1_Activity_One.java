f8b34ea98125ea0c57dd0b243c0c1f7c6a39d448
coderunnerone1 <admin@coderunner.me>
2024-01-09 11:53:34 -0800

    Update U7_L1_Activity_One.java

--- Unit-7/7.1-ArrayList/Activity1/U7_L1_Activity_One.java
+++ Unit-7/7.1-ArrayList/Activity1/U7_L1_Activity_One.java
@@ -5,13 +5,13 @@ public class U7_L1_Activity_One
 {
   public static void main(String[] args)
   {
-    // Initialize Scanner
+
     Scanner scan = new Scanner(System.in);
     
-    // Create Variable
+
     ArrayList<String> words = new ArrayList<String>();
     
-    // User Input
+
     System.out.println("Please enter words, enter STOP to stop the loop.");
     String input = scan.nextLine();
     while (!(input.equals("STOP")))
@@ -20,9 +20,9 @@ public class U7_L1_Activity_One
       input = scan.nextLine();
     }
     
-    // Final Output
+
     System.out.println("\n" + words.size() + "\n" + words);
-    /// Modify words
+
     if (words.size() > 2)
     {
       words.set(words.size() - 1, words.get(0));
